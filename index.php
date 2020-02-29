704a4aa7dd1309411422d2142891b2bdbede44e1
WildeMedia <wildemedia33@gmail.com>
2020-02-29 14:00:48 +0000

    Fixed minor errors.

--- index.php
+++ index.php
@@ -1,14 +1,13 @@
 <?php 
   
-
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
 <?php include('templates/header.php'); ?>
 
+</body>
+
 <?php include('templates/footer.php'); ?>
 
-</html>
 </html>
--- templates/footer.php
+++ templates/footer.php
@@ -1,4 +1,3 @@
 <footer class="section">
 		<div class="center grey-text">&copy; Copyright 2020 Plant Pizzas</div>
 	</footer>
-</body>
