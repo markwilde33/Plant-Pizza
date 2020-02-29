e64650767a17fcbe08acb2de0c926d602d689d1c
WildeMedia <wildemedia33@gmail.com>
2020-02-29 13:51:12 +0000

    Created templates folder with a header and footer file.

---
+++ templates/header.php
@@ -0,0 +1,19 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Plant Based Pizza</title>
+    	<!-- Compiled and minified CSS -->
+  <link rel="stylesheet" 
+   href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body class="grey lighten-4">
+	<nav class="white z-depth-0">
+    <div class="container">
+      <a href="#" class="brand-logo brand-text">Plant Pizza</a>
+      <ul id="nav-mobile" class="right hide-on-small-and-down">
+        <li><a href="#" class="btn brand z-depth-0">Add a Pizza</a></li>
+      </ul>
+    </div>
+  </nav>
+</head>
