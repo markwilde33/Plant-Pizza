e1ac3e97a908c7f3ddb429a228c5ba03a3454335
WildeMedia <wildemedia33@gmail.com>
2020-02-29 15:26:57 +0000

    Added the add.php file to serve up the add a pizza form, it's being used to return the submitted information for testing only. When the database is up and running this functionality will be removed.

---
+++ add.php
@@ -0,0 +1,33 @@
+<?php 
+
+	if(isset($_POST['submit'])){
+		echo $_POST['email'] . '<br />';
+		echo $_POST['title'] . '<br />';
+		echo $_POST['ingredients'] . '<br />';
+	}
+
+?>
+
+<!DOCTYPE html>
+<html>
+	
+	<?php include('templates/header.php'); ?>
+
+	<section class="container grey-text">
+		<h4 class="center">Add a Pizza</h4>
+		<form class="white" action="add.php" method="POST">
+			<label>Your Email</label>
+			<input type="text" name="email">
+			<label>Pizza Title</label>
+			<input type="text" name="title">
+			<label>Ingredients (please separate with commas)</label>
+			<input type="text" name="ingredients">
+			<div class="center submit-btn">
+				<input type="submit" name="submit" value="Submit" class="btn brand z-depth-0">
+			</div>
+		</form>
+	</section>
+
+	<?php include('templates/footer.php'); ?>
+
+</html>
