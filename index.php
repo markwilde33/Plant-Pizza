411c7325ec427a0909628630e8d3c1f85d549891
WildeMedia <wildemedia33@gmail.com>
2020-03-02 16:43:18 +0000

    Looped through the pizzas array to output the title and ingredients of pizzas currently stored in the database. Used materialize grid system and card class to style elements.

--- index.php
+++ index.php
@@ -23,16 +23,36 @@
 	// close connection
 	mysqli_close($conn);
 
-	print_r($pizzas);
-
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
+	
+	<?php include('templates/header.php'); ?>
 
-<?php include('templates/header.php'); ?>
+	<h4 class="center green-text">Plant Pizzas!</h4>
 
-</body>
+	<div class="container">
+		<div class="row">
 
-<?php include('templates/footer.php'); ?>
+			<?php foreach($pizzas as $pizza){ ?>
+
+				<div class="col s6 md3">
+					<div class="card z-depth-0">
+						<div class="card-content center">
+							<h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
+							<div><?php echo htmlspecialchars($pizza['ingredients']); ?></div>
+						</div>
+						<div class="card-action right-align">
+							<a class="brand-text" href="#">more info</a>
+						</div>
+					</div>
+				</div>
+
+			<?php } ?>
+
+		</div>
+	</div>
+
+	<?php include('templates/footer.php'); ?>
 
 </html>
