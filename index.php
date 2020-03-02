ff37ab6e434576b09fb0bb0b5226972db2069f93
WildeMedia <wildemedia33@gmail.com>
2020-03-02 17:41:24 +0000

    Used the PHP explode function to output the pizza ingredients as unordered list items.

--- index.php
+++ index.php
@@ -39,8 +39,14 @@
 				<div class="col s6 md3">
 					<div class="card z-depth-0">
 						<div class="card-content center">
-							<h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
-							<div><?php echo htmlspecialchars($pizza['ingredients']); ?></div>
+                            <h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
+                            <ul>
+                              <?php foreach(explode(',', $pizza['ingredients']) as $ing) {?>
+                                <li>
+                                   <?php echo htmlspecialchars($ing); ?> 
+                                </li>
+                              <?php   } ?>
+                            </ul>
 						</div>
 						<div class="card-action right-align">
 							<a class="brand-text" href="#">more info</a>
@@ -56,3 +62,4 @@
 	<?php include('templates/footer.php'); ?>
 
 </html>
+
