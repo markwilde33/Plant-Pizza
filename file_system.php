6f53336357ceae194ad945eb578f0560674f3f29
WildeMedia <wildemedia33@gmail.com>
2020-03-13 14:24:32 +0000

    Created file_system.php to run through some different common functions available when working with files in PHP.

---
+++ file_system.php
@@ -0,0 +1,35 @@
+<?php
+
+// file system - part 1
+
+// $quotes = readfile('readme.txt');
+// echo $quotes;
+
+$file = 'readme.txt';
+
+if(file_exists($file)){
+
+  //read file
+  echo readfile($file). '<br>';
+
+  // copy file
+  copy($file, 'quotes.txt');
+
+  // absolute path
+  echo realpath($file) . '<br>';
+ 
+  // file size
+  echo filesize($file) . '<br>';
+  
+  //rename file
+  rename($file, 'test.txt');
+
+  
+} else {
+ echo "no such file exists";
+}
+
+// create a new folder in the file system
+mkdir('new folder');
+
+?>
