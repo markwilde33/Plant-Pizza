73701f265ef170ecf309650bf73dedeadee1c06c
WildeMedia <wildemedia33@gmail.com>
2020-03-13 15:34:52 +0000

    Used some more advanced techniques of reading and eriting to files in PHP.

--- file_system.php
+++ file_system.php
@@ -5,31 +5,62 @@
 // $quotes = readfile('readme.txt');
 // echo $quotes;
 
-$file = 'readme.txt';
+// $file = 'readme.txt';
 
-if(file_exists($file)){
+// if(file_exists($file)){
 
-  //read file
-  echo readfile($file). '<br>';
+//   //read file
+//   echo readfile($file). '<br>';
 
-  // copy file
-  copy($file, 'quotes.txt');
+//   // copy file
+//   copy($file, 'quotes.txt');
 
-  // absolute path
-  echo realpath($file) . '<br>';
+//   // absolute path
+//   echo realpath($file) . '<br>';
  
-  // file size
-  echo filesize($file) . '<br>';
+//   // file size
+//   echo filesize($file) . '<br>';
   
-  //rename file
-  rename($file, 'test.txt');
+//   //rename file
+//   rename($file, 'test.txt');
 
   
-} else {
- echo "no such file exists";
-}
+// } else {
+//  echo "no such file exists";
+// }
 
-// create a new folder in the file system
-mkdir('new folder');
+// // create a new folder in the file system
+// // mkdir('new folder');
+
+// ?>
+
+<?php
+
+	$file = 'quotes.txt'; 
+	
+	//opening a file for reading
+	$handle = fopen($file, 'r');
+
+	//read the file
+	// echo fread($handle, filesize($file));
+	// echo fread($handle, 112);
+
+	//read a single line
+	// echo fgets($handle);
+	// echo fgets($handle);
+
+	//read a single character
+	// echo fgetc($handle);
+	// echo fgetc($handle);
+
+	// $handle = fopen($file, 'r+');
+	// $handle = fopen($file, 'a+');
+
+	// writing to a file
+	// fwrite($handle, "\nEverything popular is wrong.");
+
+	// fclose($file);	
+
+	// unlink($file);
 
 ?>
