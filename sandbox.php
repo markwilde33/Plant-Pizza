a800c89dd0af7625ec722d86f75712059e7f6cdc
WildeMedia <wildemedia33@gmail.com>
2020-03-13 10:39:57 +0000

    Practiced using sessions in sandbox and carried over the session across page by accessing it in header.php.

--- sandbox.php
+++ sandbox.php
@@ -20,7 +20,9 @@ echo $val;
 ?><br><?php
 echo $score > 40 ? 'high score!' : 'low score!';
 ?><br><?php
-// superglobals
+
+
+// super globals
 
 // $_GET['name], $_POST['name]
 
@@ -30,6 +32,18 @@ echo $score > 40 ? 'high score!' : 'low score!';
 	echo $_SERVER['PHP_SELF'] . '<br />';
 
 	// $_COOKIE, $_SESSION
+
+  // sessions, use isset to check if user has submitted the form
+  if(isset($_POST['submit'])){
+    // use the session_start function to start a session
+    session_start();
+    // use the $_SESSION super global to store the variable 'name' and set it equal to the 'name' submitted in the form
+    $_SESSION['name'] = $_POST['name'];
+    //redirect user, overwrite current form action, go to index.php when form is submitted
+    header('Location: index.php');
+
+  }
+
 
 
 ?>
@@ -43,5 +57,17 @@ echo $score > 40 ? 'high score!' : 'low score!';
 </head>
 <body>
   <p><?php echo $score > 40 ? 'high score!' : 'low score!'; ?></p>
+  <br><br>
+
+  <!-- <form action="sandbox.php"> 
+    Use the PHP super global PHP_SELF to return the page when the form is submitted instead of inputting the page
+    name, if the page name was changed, PHP_SELF would point to the new page name when the form is submitted -->
+  <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">   
+    <label for="name">enter your name</label>
+    <input type="text" name="name">
+    <input type="submit" name="submit" value="submit">
+  </form>
+
+  
 </body>
 </html>
--- templates/header.php
+++ templates/header.php
@@ -1,3 +1,23 @@
+<?php 
+  // start the session
+  session_start();
+
+  // how to override a session variable, must be stated before accessing the stored session variable, ie, above it
+  // $_SESSION['name']= 'Mr Mayagee';
+
+  //how to delete/unset a session variable
+  // if($_SERVER['QUERY_STRING'] == 'noname'){
+  //   unset($_SESSION['name']);
+  // }
+
+
+
+  // access the stored name in the session super global by setting it to a variable
+  $name = $_SESSION['name'];
+
+  
+
+?>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,6 +32,8 @@
     <div class="container">
       <strong><a href="index.php" class="left brand-logo brand-text">Plant Pizza</a></strong>
       <ul id="nav-mobile" class="right ">
+        <!-- output the stored session name -->
+        <li class="red-text text-lighten-3"><strong>Aye up, <?php echo htmlspecialchars($name); ?></strong></li>
         <li><a href="add.php" class="btn brand z-depth-0">Add a Pizza</a></li>
       </ul>
     </div>
